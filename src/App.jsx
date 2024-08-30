b7550536d31f803d2514ce350ecebd03619efc1d
Plamedi-Pindi <plamedipindi@gmail.com>
2024-08-30 11:19:59 +0100

    fixed deploy error

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './index.css';
 import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom';
 import Layout from './pages/Layout';
 import Home from './pages/Home/Home';
+import NoPage from './pages/NoPage';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Layout />}>
             <Route index element={<Home />} />
+            <Route path='*' element={<NoPage />} />
           </Route>
         </Routes>
       </Router>
