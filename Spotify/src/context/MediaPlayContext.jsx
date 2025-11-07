665722bb68224041f3b0929afd52899e2cb065ea
Plamedi-Pindi <plamedipindi77@gmail.com>
2025-11-07 12:18:23 +0100

    feat: artist recognition system

--- Spotify/src/context/MediaPlayContext.jsx
+++ Spotify/src/context/MediaPlayContext.jsx
@@ -162,7 +162,6 @@ const MediaPlayContext = ({ children }) => {
             initialSong, setInitialSong,
             loading,
             artistObjects,
-            currentArtist,
             handlePlayAudio,
             playNextAudio,
             playPreviousAudio,
